fc483199a1345c88a6897dc7ff0b16e8b404c96a
agbamajnr <ulimhunyieagbama@gmail.com>
2021-10-07 22:04:02 +0100

    make it better

--- public/script.js
+++ public/script.js
@@ -1,9 +1,9 @@
-const socket = io('/room')
+const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
-  port: '3030'
+  port: '443'
 })
 
 
